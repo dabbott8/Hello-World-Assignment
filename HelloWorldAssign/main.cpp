b70f315747b3a3ec7d6993409bd37af8f5d0cc3d
dabbott8 <35750485+dabbott8@users.noreply.github.com>
2018-02-01 12:12:54 -0600

    Add files via upload
    the hello world project

---
+++ HelloWorldAssign/main.cpp
@@ -0,0 +1,18 @@
+//Dylan Abbott
+//ITSE1307
+//Hello World
+//A simple hello world
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main ()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
